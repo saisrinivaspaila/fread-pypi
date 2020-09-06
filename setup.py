d4a0170274d885de47b47cf4c61cadb7b070021f
saisrinivaspaila <pailasaisrinivas7668@gmail.com>
2020-09-06 07:48:34 +0530

    some changes

--- setup.py
+++ setup.py
@@ -1,15 +1,22 @@
-from setuptools import setup, find_packages
+from distutils.core import setup
 setup(
-    author="Sai Srinivas Paila",
-    author_email='pailasaisrinivas7668@gmail.com',
-    classifiers=[
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.7',
-    ],
-    description="Getting inputs from input.txt",
-    license="MIT license",
-    include_package_data=True,
     name='fileInput',
-    version='0.1.0',
-    zip_safe=False,
+    packages=['fileInput'],
+    version='0.1',
+    license='MIT',
+    description='Change from interaction console to FileInput based console.',
+    author='Venkata Sai Srinivsa Paila',
+    author_email='pailasaisrinivas7668@gmail.comm',
+    url='https://github.com/saisrinivaspaila/FileInput-python-package',
+    download_url='https://github.com/saisrinivaspaila/FileInput-python-package/archive/V_0.1.tar.gz',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
 )
