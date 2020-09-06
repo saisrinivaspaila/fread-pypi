f9879449764d3f883be4f449ed68712bbfb86605
saisrinivaspaila <pailasaisrinivas7668@gmail.com>
2020-09-06 09:15:23 +0530

    Changes Done

--- README.md
+++
@@ -1,1 +0,0 @@
-# FileInput-python-package
---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='fread',
+    version='0.0.1',
+    license='MIT',
+    description='Change from interaction console to FileInput based console.',
+    author='Venkata Sai Srinivsa Paila',
+    author_email='pailasaisrinivas7668@gmail.comm',
+    url='',
+    keywords=['input', 'file', 'programming'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Education',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
